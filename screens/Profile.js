f38568d1f403caf36724d44b5dc4a9fa9e6a3bb9
balanabhinav <abhinavbalan2302@gmail.com>
2022-11-01 13:54:12 -0400

    added profile page

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,14 +1,135 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image, TouchableOpacity,ScrollView,Button} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Icon from "react-native-vector-icons/FontAwesome";
+import React from 'react';
 
 const Profile = () => {
   return (
+    <SafeAreaView>
     <View>
-      <Text>Profile</Text>
+    <Image
+            style={styles.profileImage}
+            resizeMode="contain"
+            source={require("../assets/profileImage.jpg")}
+          /> 
+     <Text style={styles.profile}>
+      Antonello Messi
+      </Text>    
+      <Text style={styles.profiletext}>
+      antonellomessi@gmail.com
+      </Text>
+      <View
+      style={{
+      borderBottomColor: 'black',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      marginTop:20
+  }}
+/>
+      <View style ={{flexDirection: 'row'}}> 
+      <Icon style={{marginTop: 30, marginLeft:15}} name="user" size={20} color="#4169E1" /> 
+      <Text style={styles.emailtext}>My Profile</Text>
+      </View>
+
+      <View
+      style={{
+      borderBottomColor: 'black',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      marginTop:20
+  }}
+/>
+
+<View style ={{flexDirection: 'row'}}> 
+      <Icon style={{marginTop: 30, marginLeft:15}} name="cog" size={20} color="#4169E1" /> 
+      <Text style={styles.emailtext}>Settings</Text>
+      </View>
+
+      <View
+      style={{
+      borderBottomColor: 'black',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      marginTop:20
+  }}
+/>
+<View style ={{flexDirection: 'row'}}> 
+      <Icon style={{marginTop: 30, marginLeft:15}} name="comments" size={20} color="#4169E1" /> 
+      <Text style={styles.emailtext}>Frequently Asked Questions</Text>
+      </View>
+
+      <View
+      style={{
+      borderBottomColor: 'black',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      marginTop:20
+  }}
+/>
+<View style ={{flexDirection: 'row'}}> 
+      <Icon style={{marginTop: 30, marginLeft:15}} name="envelope" size={20} color="#4169E1" /> 
+      <Text style={styles.emailtext}>Contact Us</Text>
+      </View>
+
+      <View
+      style={{
+      borderBottomColor: 'black',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      marginTop:20
+  }}
+/>
+<View style ={{flexDirection: 'row'}}> 
+      <Icon style={{marginTop: 30, marginLeft:15}} name="phone" size={20} color="#4169E1" /> 
+      <Text style={styles.emailtext}>Support</Text>
+      </View>
+
+      <View
+      style={{
+      borderBottomColor: 'black',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      marginTop:20
+  }}
+/>
+<View style ={{flexDirection: 'row'}}> 
+      <Icon style={{marginTop: 30, marginLeft:15}} name="sign-out" size={20} color="#4169E1" /> 
+      <Text style={styles.emailtext}>Logout</Text>
+      </View>
+
     </View>
+    </SafeAreaView>
   )
 }
 
 export default Profile
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  profileImage: {
+    borderRadius: 60,
+    height: 120,
+    width: 120,
+    marginTop:75,
+    marginLeft:130,
+    alignContent: "center"
+    
+  },
+  profile: {
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 10,
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+
+  profiletext: {
+    fontSize: 16,
+    fontWeight:"300",
+    textAlign: "center"
+  },
+
+  emailtext: {
+    fontSize: 16,
+    marginTop: 30,
+    marginLeft: 10,
+    fontWeight: "600"
+  }
+
+ 
+})
